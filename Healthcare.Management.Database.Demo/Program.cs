d175a66cd63376ca4e41b0d9d3f71e7c76705bbf
NetNinjaEngineer <memo5260287@outlook.com>
2024-04-06 15:49:49 +0200

    EmployeePerformanceSummary

--- Healthcare.Management.Database.Demo/Program.cs
+++ Healthcare.Management.Database.Demo/Program.cs
@@ -8,7 +8,11 @@ namespace Healthcare.Management.Database.Demo
         {
             using (var context = new ApplicationDbContext())
             {
-                RunAppointmentHistoryView(context);
+                //RunAppointmentHistoryView(context);
+
+                //GetTotalAppointmentsForDepartments(context);
+
+                EmployeePerformanceSummary(context);
 
             }
 
@@ -31,6 +35,25 @@ namespace Healthcare.Management.Database.Demo
                                                  LastAppointment = paGroup.Max(x => x.appointment.AppointmentDate)
                                              }).ToList();
 
+            var queryFluentSyntax = context.Patients
+                .Join(
+                    context.Appointments,
+                    patient => patient.Id,
+                    appointment => appointment.PatientId,
+                    (patient, appointment) => new { patient, appointment }
+                )
+                .GroupBy(
+                    x => new { x.patient.FirstName, x.patient.LastName },
+                    (key, group) => new
+                    {
+                        PatientName = string.Concat(key.FirstName, " ", key.LastName),
+                        TotalAppointments = group.Count(x => x.appointment != null),
+                        FirstAppointment = group.Min(x => x.appointment.AppointmentDate),
+                        LastAppointment = group.Max(x => x.appointment.AppointmentDate)
+                    }
+                );
+
+
             foreach (var paHistory in patientAppointmentHistory)
                 Console.WriteLine(
                     $"\nPatientName: {paHistory?.PatientName}" +
@@ -40,5 +63,63 @@ namespace Healthcare.Management.Database.Demo
 
 
         }
+
+        public static void GetTotalAppointmentsForDepartments(ApplicationDbContext context)
+        {
+            var query = (from department in context.Departments
+                         join employee in context.Employees
+                         on department.Id equals employee.DepartmentId
+                         join appointment in context.Appointments
+                         on employee.Id equals appointment.EmployeeId
+                         where appointment.AppointmentDate == DateOnly.FromDateTime(DateTime.Now)
+                         group
+                         new { department, employee, appointment }
+                            by department.Name into grouped
+                         select new
+                         {
+                             Department = grouped.Key,
+                             TotalAppointments =
+                                grouped.Where(
+                                    x => x.appointment != null).Count()
+
+                         }).ToList();
+
+
+            foreach (var item in query)
+                Console.WriteLine(
+                    $"\nDepartment: {item.Department}" +
+                    $"\nTotalAppointments: {item?.TotalAppointments}");
+
+        }
+
+        public static void EmployeePerformanceSummary(ApplicationDbContext context)
+        {
+            var employeePerformanceSummary = from appointment in context.Appointments
+                                             join employee in context.Employees
+                                             on appointment.EmployeeId equals employee.Id
+                                             join patient in context.Patients
+                                             on appointment.PatientId equals patient.Id
+                                             where employee.JobTitle!.ToLower() == "doctor"
+                                             group new { appointment, employee, patient } by
+                                             new { employee.FirstName, employee.LastName, employee.JobTitle }
+                                             into grouped
+                                             select new
+                                             {
+                                                 Employee = string.Concat(grouped.Key.FirstName, ' ', grouped.Key.LastName),
+                                                 TotalAppointments = grouped.Count(x => x.appointment != null),
+                                                 EmployeeJobTitle = grouped.Key.JobTitle,
+                                                 Patients = grouped.Select(x =>
+                                                    string.Concat(x.patient.FirstName, ' ', x.patient.LastName)).ToList()
+                                             };
+            foreach (var empPerformance in employeePerformanceSummary)
+                Console.WriteLine(
+                    $"\n{empPerformance.Employee} ({empPerformance.EmployeeJobTitle})" +
+                    $"\nTotalAppointments: {empPerformance?.TotalAppointments}" +
+                    $"\nPatients: {string.Join(", ", empPerformance!.Patients)}");
+
+
+
+        }
+
     }
 }
